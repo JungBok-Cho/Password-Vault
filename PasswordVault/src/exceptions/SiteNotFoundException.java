5102d85f36959631b59457c93f3e565d5438d7a0
JungBok-Cho <55774376+JungBok-Cho@users.noreply.github.com>
2020-06-11 17:58:25 -0700

    Modify the code in SiteNotFoundException.java

--- PasswordVault/src/exceptions/SiteNotFoundException.java
+++ PasswordVault/src/exceptions/SiteNotFoundException.java
@@ -1,9 +1,9 @@
 /*
  * JungBok Cho
- * CPSC 5011, Seattle University
- * This is free and unencumbered software released into the public domain.
+ * Password vault system
  */
 package exceptions;
+
 /**
  * This is a program to throw SiteNotFoundException
  * 
@@ -18,7 +18,7 @@ public class SiteNotFoundException extends Exception {
 	 * Error message
 	 */
 	public SiteNotFoundException() {
-        super("Error: Site Not Found");
-    }
+        	super("Error: Site Not Found");
+        }
 
 }
